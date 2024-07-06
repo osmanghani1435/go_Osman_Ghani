2ff64948ba215560984b99497d7ab796304b4757
osmanghani1435 <osmanghani.a143@gmail.com>
2024-07-06 12:48:24 -0700

    for fun

---
+++ self-fun/fun.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	Love1 := "Osman> := O"
+	love2 := "Asiya> := A"
+	love3 := Love1 + " " + love2
+	fmt.Println(love3)
+	fmt.Println(" its Means := O + A")
+	fmt.Println("Final difinition:=> O Love A")
+	fmt.Println("Protec Those You Love")
+	fmt.Println("This is just for fun by: Osman Ghani")
+	fmt.Println("if any Professional reviewing")
+	fmt.Println("Please don't mind")
+}
