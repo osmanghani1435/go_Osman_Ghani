d513e6139082324492b97014e11175fba2323506
osmanghani1435 <osmanghani.a143@gmail.com>
2024-07-06 04:46:17 -0700

    hello world

---
+++ go-go/ifelse.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := 4
+	if number%2 == 0 {
+		fmt.Println("correct")
+	} else {
+		fmt.Println("incorrect")
+	}
+
+	// Additional if statement for demonstration
+	// if number%2 == 0 {
+	//     fmt.Println("correct")
+	// } else {
+	//     fmt.Println("incorrect")
+	// }
+}
